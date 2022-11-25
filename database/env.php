e438baf891434c797692f2b2a02d9151bf4e361d
mdparvej123 <pj.parvaz45@gmail.com>
2022-11-25 23:16:58 +0600

    first commit

---
+++ database/env.php
@@ -0,0 +1,8 @@
+<?php
+$hostName = 'localhost';
+$userName = 'root';
+$password = '';
+$dataBase = 'cit_yummy_project';
+$connection = mysqli_connect("hostname", "username", "password", "database");
+
+?>
